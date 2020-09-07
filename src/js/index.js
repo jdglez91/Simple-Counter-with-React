82f919d0b456213cee07023c6ba00cb1ec3d8f00
jdglez91 <jdglez1991@gmail.com>
2020-09-07 15:47:23 +0000

    not complete

--- src/js/index.js
+++ src/js/index.js
@@ -7,43 +7,70 @@ import "bootstrap";
 
 //include your index.scss file into the bundle
 import "../styles/index.scss";
+import PropTypes from "prop-types";
 
 //import your own components
 function SimpleCounter(props) {
 	let mystyle = {
 		color: "white",
-		backgroundColor: "gray",
-		padding: "10px",
+		backgroundColor: "#1b1717",
+		padding: "0",
 		fontFamily: "Arial",
-		fontSize: "40px "
+		fontSize: "70px ",
+		borderRadius: "30px"
 	};
 	return (
 		<div className="container">
-			<div className="row text-center " style={{ background: "black" }}>
-				<div className="col-sm pt-5 mr-3">
+			<div
+				className="row text-center "
+				style={{ background: "black", padding: "20px" }}>
+				<div
+					className="col-sm pt-5 mr-3"
+					style={{ background: "#1b1717", borderRadius: "30px" }}>
 					<i className="far fa-clock fa-3x " />
 				</div>
 				<div className="col-sm pt-5 mr-3" style={mystyle}>
-					6
+					{props.digitSix}
 				</div>
 				<div className="col-sm pt-5 mr-3" style={mystyle}>
-					5
+					{props.digitFive}
 				</div>
 				<div className="col-sm pt-5 mr-3" style={mystyle}>
-					4
+					{props.digitFour}
 				</div>
 				<div className="col-sm pt-5 mr-3" style={mystyle}>
-					3
+					{props.digitThree}
 				</div>
 				<div className="col-sm pt-5 mr-3" style={mystyle}>
-					2
+					{props.digitTwo}
 				</div>
 				<div className="col-sm pt-5 mr-3" style={mystyle}>
-					1
+					{props.digitOne}
 				</div>
 			</div>
 		</div>
 	);
 }
-//render your react application
-ReactDOM.render(<SimpleCounter />, document.querySelector("#app"));
+SimpleCounter.propTypes = {
+	digitSix: PropTypes.number,
+	digitFive: PropTypes.number,
+	digitFour: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitOne: PropTypes.number
+};
+let counter = 0;
+setInterval(() => {
+	const six = Math.floor(counter / 100000);
+
+	const five = Math.floor(counter / 10000);
+	const four = Math.floor(counter / 1000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+	ReactDOM.render(
+		<SimpleCounter digitOne={counter.indexOf()} />,
+		document.querySelector("#app")
+	);
+	counter++;
+}, 1000);
